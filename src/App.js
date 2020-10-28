9ba6bc1f83779076b1c9bdda1e92a6ac27361f3b
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-27 20:29:33 -0600

    switched to class component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import React from "react";
 import PersonalInfo from "./formcomponents/personalInfo";
 
-function App() {
-  return (
-    <form>
-      <div>
-         <PersonalInfo />
-      </div>
-    </form>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <PersonalInfo/>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default App;
