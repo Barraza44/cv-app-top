8d246e1e50ab05363aec2f754048fb8cec62a503
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-27 22:11:02 -0600

    added second form component

---
+++ src/formcomponents/schoolInfo.js
@@ -0,0 +1,13 @@
+import React from  "react";
+
+class SchoolInfo extends React.Component{
+  render() {
+    return(
+      <div>
+        HEY
+      </div>
+    );
+  }
+}
+
+export default SchoolInfo;
