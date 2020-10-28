a5246f6bbf8c86028d349969d9f00d555b756332
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-27 21:49:11 -0600

    added handle change method

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import React from "react";
 import PersonalInfo from "./formcomponents/personalInfo";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      names: "",
+      address: "",
+      email: "",
+      phone: ""
+    }
+    this.handleChange.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <form>
         <div>
-          <PersonalInfo/>
+          <PersonalInfo state={this.state} handleChange={this.handleChange}/>
         </div>
       </form>
     );
--- src/formcomponents/personalInfo.js
+++ src/formcomponents/personalInfo.js
@@ -5,19 +5,22 @@ class PersonalInfo extends React.Component{
     super(props);
   }
   render() {
+    const {names, address, email, phone} = this.props.state;
+    const {handleChange} = this.props;
+
     return(
       <div>
         <label htmlFor="cv-name">Your name</label>
-        <input type="text" name="name" id="cv-name"/>
+        <input type="text" name="names" id="cv-name" onChange={handleChange}/>
 
         <label htmlFor="cv-address">Address</label>
-        <input type="text" name="address" id="cv-address"/>
+        <input type="text" name="address" id="cv-address" onChange={handleChange}/>
 
         <label htmlFor="cv-email">Your email</label>
-        <input type="email" name="email" id="cv-email"/>
+        <input type="email" name="email" id="cv-email" onChange={handleChange}/>
 
         <label htmlFor="cv-phone">Your phone</label>
-        <input type="tel" name="phone" id="cv-phone"/>
+        <input type="tel" name="phone" id="cv-phone" onChange={handleChange}/>
       </div>
     );
   }
