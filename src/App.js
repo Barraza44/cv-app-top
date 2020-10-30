5c6ae8ad755f19bfd503ba5f3640f936b53baa8d
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-29 22:28:47 -0600

    added components that display the form data, allowed edit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import PersonalInfo from "./formcomponents/personalInfo";
 import SchoolInfo from "./formcomponents/schoolInfo";
 import WorkInfo from "./formcomponents/workInfo";
 
+import PersonalInfoDisplay from "./displayComponents/personalInfoDisplay";
+import SchoolInfoDisplay from "./displayComponents/schoolInfoDisplay";
+import WorkInfoDisplay from "./displayComponents/workInfoDisplay";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -21,10 +25,11 @@ class App extends React.Component {
       tasks:"",
       workStart:"",
       workEnd: "",
-      editing: false
+      editing: true
     }
     this.handleChange.bind(this);
     this.handleSubmit.bind(this);
+    this.handleEdit.bind(this);
   }
 
   handleChange = (e) => {
@@ -36,20 +41,41 @@ class App extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.setState({
+      editing: false
+    })
+  }
+
+  handleEdit = () => {
+    this.setState({
       editing: true
     })
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
+    const editing = this.state.editing;
+    if(editing){
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <PersonalInfo state={this.state} handleChange={this.handleChange}/>
+            <SchoolInfo state={this.state} handleChange={this.handleChange}/>
+            <WorkInfo state={this.state} handleChange={this.handleChange} />
+            <button type={"submit"}>Send!</button>
+          </div>
+        </form>
+      );
+    }
+    else {
+      return (
         <div>
-          <PersonalInfo state={this.state} handleChange={this.handleChange}/>
-          <SchoolInfo state={this.state} handleChange={this.handleChange}/>
-          <WorkInfo state={this.state} handleChange={this.handleChange} />
+        <PersonalInfoDisplay data={this.state} />
+        <SchoolInfoDisplay data={this.state} />
+        <WorkInfoDisplay data={this.state} />
+        <button onClick={this.handleEdit}>Edit</button>
         </div>
-      </form>
-    );
+      );
+    }
+
   }
 }
 
